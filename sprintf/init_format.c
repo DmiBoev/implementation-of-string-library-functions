bd8f75c1ae9d64f236fec53214aa5ae607f46f71
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 16:40:53 +0300

    Create init_format.c

---
+++ sprintf/init_format.c
@@ -0,0 +1,17 @@
+#include "my_string.h"
+
+format init_format(void) {
+  s21_format fmt;
+  fmt.minus = 0;
+  fmt.plus = 0;
+  fmt.space = 0;
+  fmt.sharp = 0;
+  fmt.zero = 0;
+  fmt.width = 0;
+  fmt.precision = 0;
+  fmt.precision_set = 0;
+  fmt.length = 0;
+  fmt.spec = 0;
+
+  return fmt;
+}
