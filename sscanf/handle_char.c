aac0de71407d4bd45b83af93fec53cb07b337802
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 13:52:44 +0300

    Update handle_char.c

--- sscanf/handle_char.c
+++ sscanf/handle_char.c
@@ -1,4 +1,5 @@
 //Processing a single character case (c)
+#include "my_string.h"
 int handle_char(const char** src, va_list* args, Token tok) {
   if (!**src) return 0;
   if (!tok.star) *(va_arg(*args, char*)) = **src;
