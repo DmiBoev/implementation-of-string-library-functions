885257503c3582a7c4617c756e42802e79015066
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 16:54:37 +0300

    Create to_lower.c

---
+++ special_string_func/to_lower.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "my_string.h"
+
+void* to_lower(const char* str) {
+  char* res = NULL;
+  if (str) {
+    my_size len = strlen(str);
+    if ((res = malloc((len + 1) * sizeof(char)))) {
+      for (my_size i = 0; i <= len; i++) {
+        res[i] = (str[i] >= 'A' && str[i] <= 'Z') ? str[i] + 32 : str[i];
+      }
+    }
+  }
+  return (void*)res;
+}
