90ff7448acd05dbe59c8afe6862d56f7abce1c7a
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 13:46:43 +0300

    Create handle_string.c

---
+++ sscanf/handle_string.c
@@ -0,0 +1,20 @@
+//6. Processing of the string case (s) 
+#include "my_string.h"
+
+int handle_string(const char** src, va_list* args, Token tok, int* count,
+                  int success_match) {
+  propusk_simvols(src);
+  if (!**src) {
+    if (!success_match) *count = -1;
+    return 0;
+  }
+  char* p = tok.star ? (void*)0 : va_arg(*args, char*);
+  int rd = 0;
+  while (**src && !isspace(**src) && (tok.width == 0 || rd < tok.width)) {
+    if (p) *p++ = **src;
+    (*src)++;
+    rd++;
+  }
+  if (p) *p = '\0';
+  return 1;
+}
