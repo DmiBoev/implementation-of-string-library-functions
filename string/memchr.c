628119c86eec13db69171fb5b08f2dcf04e7bfa4
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 12:36:19 +0300

    Update memchr.c

--- string/memchr.c
+++ string/memchr.c
@@ -1,10 +1,10 @@
 // memchr - find a byte in memory
 #include "my_string.h"
 
-void* s21_memchr(const void* str, int c, s21_size n) {
+void* my_memchr(const void* str, int c, my_size n) {
   const unsigned char* s = (const unsigned char*)str;
-  void* target = S21_NULL;
-  for (s21_size i = 0; i < n && target == S21_NULL; i++) {
+  void* target = NULL;
+  for (my_size i = 0; i < n && target == NULL; i++) {
     if (s[i] == (char)c) {
       target = (void*)&s[i];
     }
