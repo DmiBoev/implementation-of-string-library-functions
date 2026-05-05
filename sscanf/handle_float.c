3e8d3e1cda7cbf25661e0c97cb36aadc8a141e18
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-05 13:45:23 +0300

    Create handle_float.c

---
+++ sscanf/handle_float.c
@@ -0,0 +1,13 @@
+//Processing cases of fractional numbers (f, e, E, g, G)
+#include "my_string.h"
+
+int handle_float(const char** src, va_list* args, Token tok) {
+  propusk_simvols(src);
+  long double val;
+  int res = 0;
+  if (parse_float(src, &val, tok.width)) {
+    if (!tok.star) write_float(args, val, tok);
+    res = 1;
+  }
+  return res;
+}
