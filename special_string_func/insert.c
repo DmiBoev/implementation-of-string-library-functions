b8533f0213a0b00f38568f6020689da15a48beda
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 08:21:39 +0300

    Update insert.c

--- special_string_func/insert.c
+++ special_string_func/insert.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include "my_string.h"
 
-void* insert(const char* src, const char* str,
+void* my_insert(const char* src, const char* str,
              my_size start_index) { 
-  my_size src_len = (src) ? strlen(src) : 0;
-  my_size str_len = (str) ? strlen(str) : 0;
-  char* res = S21_NULL;
+  my_size src_len = (src) ? my_strlen(src) : 0;
+  my_size str_len = (str) ? my_strlen(str) : 0;
+  char* res = NULL;
 
   if (src && str && start_index <= src_len) {
     res = malloc((src_len + str_len + 1) * sizeof(char));
     if (res) {
-      memcpy(res, src, start_index);
-      memcpy(res + start_index, str, str_len);
-      memcpy(res + start_index + str_len, src + start_index,
+      my_memcpy(res, src, start_index);
+      my_memcpy(res + start_index, str, str_len);
+      my_memcpy(res + start_index + str_len, src + start_index,
                  src_len - start_index);
       res[src_len + str_len] = '\0';
     }
