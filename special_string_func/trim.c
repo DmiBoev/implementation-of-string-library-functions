4405c6e16805c6163132c32cffaf6b2a98e94f82
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 08:23:09 +0300

    Update trim.c

--- special_string_func/trim.c
+++ special_string_func/trim.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include "my_string.h"
 
-void* trim(
+void* my_trim(
     const char* src,
     const char* trim_chars) { 
   char* res = NULL;
   if (src && trim_chars) {
     const char* start = src;
-    const char* end = src + strlen(src) - 1;
+    const char* end = src + my_strlen(src) - 1;
 
-    while (*start != '\0' && strchr(trim_chars, *start)) start++;
-    while (end >= start && strchr(trim_chars, *end)) end--;
+    while (*start != '\0' && my_strchr(trim_chars, *start)) start++;
+    while (end >= start && my_strchr(trim_chars, *end)) end--;
 
     my_size len_res = (end >= start) ? end - start + 1 : 0;
     res = malloc((len_res + 1) * sizeof(char));
     if (res) {
-      memcpy(res, start, len_res);
+      my_memcpy(res, start, len_res);
       res[len_res] = '\0';
     }
   }
