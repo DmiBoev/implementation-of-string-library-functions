8191a5e960acc17c927c4db3c280bbc4520ccdb8
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 08:31:22 +0300

    Update my_string.h

--- my_string.h
+++ my_string.h
@@ -1,5 +1,5 @@
-#ifndef STRING_H
-#define STRING_H
+#ifndef MY_STRING_H
+#define MY_STRING_H
 
 #include <ctype.h>
 #include <stdarg.h>
@@ -27,10 +27,10 @@ typedef struct {
 
 typedef size_t my_size;
 
-void *memchr(const void *str, int c, s21_size n);
-int memcmp(const void *str1, const void *str2, my_size n);
-void *memcpy(void *dest, const void *src, my_size n);
-void *memset(void *str, int c, my_size n);
+void *my_memchr(const void *str, int c, s21_size n);
+int my_memcmp(const void *str1, const void *str2, my_size n);
+void *my_memcpy(void *dest, const void *src, my_size n);
+void *my_memset(void *str, int c, my_size n);
 
 void apply_string_precision(char *buffer, format fmt);
 void long_to_str(long num, char *buffer);
@@ -39,23 +39,23 @@ void apply_int_precision(char *buffer, format fmt);
 void apply_alt_form(char *buffer, format fmt, unsigned long num);
 void apply_pointer_prefix(char *buffer);
 
-char *strncat(char *dest, const char *src, size n);
-char *strchr(const char *str, int c);
-int strncmp(const char *str1, const char *str2, size n);
-char *strncpy(char *dest, const char *src, size n);
-s21_size strcspn(const char *str1, const char *str2);
-char *strerror(int errnum);
-s21_size strlen(const char *str);
-char *strpbrk(const char *str1, const char *str2);
-char *strrchr(const char *str, int c);
-char *strstr(const char *haystack, const char *needle);
-char *strtok(char *str, const char *delim);
+char *my_strncat(char *dest, const char *src, size n);
+char *my_strchr(const char *str, int c);
+int my_strncmp(const char *str1, const char *str2, size n);
+char *my_strncpy(char *dest, const char *src, size n);
+s21_size my_strcspn(const char *str1, const char *str2);
+char *my_strerror(int errnum);
+s21_size my_strlen(const char *str);
+char *my_strpbrk(const char *str1, const char *str2);
+char *my_strrchr(const char *str, int c);
+char *my_strstr(const char *haystack, const char *needle);
+char *my_strtok(char *str, const char *delim);
 void ulong_to_str(unsigned long num, char *str);
 void general_to_str(long double num, char *str, format fmt);
-int sprintf(char *str, const char *format, ...);
+int my_sprintf(char *str, const char *format, ...);
 int get_exp(long double num);
 unsigned long get_unsigned_arg(va_list *args, format fmt);
-int sscanf(const char *src, const char *format, ...);
+int my_sscanf(const char *src, const char *format, ...);
 long get_signed_arg(va_list *args, format fmt);
 int get_digit(char c, int base);
 void propusk_simvols(const char **src);
@@ -73,12 +73,12 @@ int handle_string(const char **src, va_list *args, Token tok, int *count,
                   int success_match);
 int handle_char(const char **src, va_list *args, Token tok);
 format init_format(void);
-void *insert(const char *src, const char *str, size start_index);
-void *to_lower(const char *str);
-void *to_upper(const char *str);
-void *trim(const char *src, const char *trim_chars);
-int s21_is_digit(char c);
-const char *s21_parse_format(const char *format, format *fmt,
+void *my_insert(const char *src, const char *str, size start_index);
+void *my_to_lower(const char *str);
+void *my_to_upper(const char *str);
+void *my_trim(const char *src, const char *trim_chars);
+int is_digit(char c);
+const char *parse_format(const char *format, format *fmt,
                              va_list *args);
 void ulong_to_str(unsigned long num, char *str);
 void remove_trailing_zeros(char *str);
