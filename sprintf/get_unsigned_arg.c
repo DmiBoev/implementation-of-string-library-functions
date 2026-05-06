20548d19f115fd6a39c63da6700265cc68c22008
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 10:37:37 +0300

    Update get_unsigned_arg.c

--- sprintf/get_unsigned_arg.c
+++ sprintf/get_unsigned_arg.c
@@ -1,6 +1,6 @@
 #include "my_string.h"
 
-unsigned long get_unsigned_arg(va_list* args, format fmt) {
+unsigned long get_unsigned_arg(va_list* args, my_format fmt) {
   unsigned long value = 0;
   if (fmt.length == 'h') {
     value = (unsigned short)va_arg(*args, unsigned int);
