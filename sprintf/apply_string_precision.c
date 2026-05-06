abbf9cffb278c1d8916d1c7358f4912dd6358ca5
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 10:36:44 +0300

    Update apply_string_precision.c

--- sprintf/apply_string_precision.c
+++ sprintf/apply_string_precision.c
@@ -1,5 +1,5 @@
 #include "my_string.h"
-void apply_string_precision(char* buffer, format fmt) {
+void apply_string_precision(char* buffer, my_format fmt) {
   if (fmt.precision_set) {
     int i = 0;
     while (buffer[i] != '\0' && i < fmt.precision) {
