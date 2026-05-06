ec2416e4c5e29429145528c3938b4b103c57a3ad
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 08:22:06 +0300

    Update to_lower.c

--- special_string_func/to_lower.c
+++ special_string_func/to_lower.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "my_string.h"
 
-void* to_lower(const char* str) {
+void* my_to_lower(const char* str) {
   char* res = NULL;
   if (str) {
-    my_size len = strlen(str);
+    my_size len = my_strlen(str);
     if ((res = malloc((len + 1) * sizeof(char)))) {
       for (my_size i = 0; i <= len; i++) {
         res[i] = (str[i] >= 'A' && str[i] <= 'Z') ? str[i] + 32 : str[i];
