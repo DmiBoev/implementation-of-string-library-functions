9bac45faf6f09911302947c8241e48d48ebe6626
Dmitry Boev <dima_boev_01@mail.ru>
2026-05-06 08:22:29 +0300

    Update to_upper.c

--- special_string_func/to_upper.c
+++ special_string_func/to_upper.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "my_string.h"
 
-void* to_upper(const char* str) { 
+void* my_to_upper(const char* str) { 
   char* res = NULL;
   if (str) {
-    my_size len = strlen(str);
+    my_size len = my_strlen(str);
     if ((res = malloc((len + 1) * sizeof(char)))) {
       for (s21_size i = 0; i <= len; i++) {
         res[i] = (str[i] >= 'a' && str[i] <= 'z') ? str[i] - 32 : str[i];
